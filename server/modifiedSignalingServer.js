e291589814673342117854af79836cb9305a1c2f
ojasvi264 <lalamdorje@gmail.com>
2025-02-03 17:10:17 +0900

    refractor[signalingServer]:modifies according to the new changes.

--- server/modifiedSignalingServer.js
+++ server/modifiedSignalingServer.js
@@ -7,27 +7,20 @@ let receivers = []; // Store receiver connections
 server.on('connection', (socket) => {
     socket.on('message', (message) => {
         const data = JSON.parse(message.toString());
-
         if (data.role === "sender") {
             senders.push(socket);
             console.log("New sender connected.");
         } else if (data.role === "receiver") {
             receivers.push(socket);
             console.log("New receiver connected.");
-
-            senders.forEach(sender => {
-                sender.send(JSON.stringify({
-                    offer: sender.offer,
-                    mediaConfig: sender.mediaConfig // Send mediaConfig to the receiver
-                }));
-            });
-        } else if (data.offer) {
+        } 
+        if (data.offer) {
             socket.offer = data.offer; // Store sender's offer
             socket.mediaConfig = data.mediaConfig; // Store the sender's mediaConfig
-
-            // Forward the offer and mediaConfig to all receivers
             receivers.forEach(receiver => {
                 receiver.send(JSON.stringify({
+                    senderName: data.name,
+                    senderId: data.senderId,
                     offer: data.offer,
                     mediaConfig: data.mediaConfig // Include mediaConfig with the offer
                 }));
@@ -37,8 +30,15 @@ server.on('connection', (socket) => {
                 sender.send(JSON.stringify({ answer: data.answer }));
             });
         } else if (data.iceCandidate) {
-            senders.forEach(sender => sender.send(JSON.stringify({ iceCandidate: data.iceCandidate })));
-            receivers.forEach(receiver => receiver.send(JSON.stringify({ iceCandidate: data.iceCandidate })));
+            senderId = data.senderId;
+            // console.log(data.iceCandidate);
+            if(senderId){
+                receivers.forEach(receiver => receiver.send(JSON.stringify({ iceCandidate: data.iceCandidate, senderId: senderId})));               
+            } 
+            else {
+                // console.log(data);
+                senders.forEach(sender => sender.send(JSON.stringify({ iceCandidate: data.iceCandidate })));
+            }
         }
     });
 
